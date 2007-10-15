c3f5c93d00a46fce8ae615bf81f11fd635a94871
Stephen Green <stephen.green@sun.com>
2007-10-15 10:29:12 -0400

    Testing adding a new Netbeans project to the repository.

---
+++ poc/TestApp/src/testapp/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Main.java
+ * 
+ * Created on Oct 15, 2007, 10:27:41 AM
+ * 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testapp;
+
+/**
+ *
+ * @author stgreen
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
