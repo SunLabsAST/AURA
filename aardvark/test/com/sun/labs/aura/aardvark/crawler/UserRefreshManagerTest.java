f169bae6c78e41e06a12d4125cd3783eccf13059
Paul Lamere <Paul.Lamere@sun.com>
2007-10-31 10:36:10 -0400

    Added user tests

---
+++ aardvark/test/com/sun/labs/aura/aardvark/crawler/UserRefreshManagerTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.labs.aura.aardvark.crawler;
+
+import com.sun.labs.aura.aardvark.store.item.User;
+import edu.cmu.sphinx.util.props.PropertySheet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author plamere
+ */
+public class UserRefreshManagerTest {
+
+    public UserRefreshManagerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test public void testJunit() {
+        assertTrue("Testing Junit, this should pass", true);
+    }
+
+//    @Test public void ifAtFirstYouDontSucceedYouFail() {
+//        assertTrue("Testing Junit, this should fail", false);
+//    }
+}
