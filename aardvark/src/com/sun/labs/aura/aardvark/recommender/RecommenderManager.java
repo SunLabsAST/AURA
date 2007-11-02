dae19e21c4f65ada24c368899835ef29a20cd5a7
Stephen Green <stephen.green@sun.com>
2007-11-02 11:25:47 -0400

    Added the com.sun.labs.aura.aardvark.recommender package and the RecommenderManager interface to same.

---
+++ aardvark/src/com/sun/labs/aura/aardvark/recommender/RecommenderManager.java
@@ -0,0 +1,19 @@
+package com.sun.labs.aura.aardvark.recommender;
+
+import com.sun.labs.aura.aardvark.store.item.Entry;
+import com.sun.labs.aura.aardvark.store.item.User;
+import com.sun.labs.util.props.Configurable;
+import java.util.List;
+
+/**
+ * An interface for managing the recommenders used by Aardvark.
+ */
+public interface RecommenderManager extends Configurable {
+
+    /**
+     * Gets a list of recommendations for a user.
+     * @param user the user that we want recommendations for
+     * @return a list of the entries that we want to recommend to the user.
+     */
+    List<Entry> getRecommendations(User user);
+}
