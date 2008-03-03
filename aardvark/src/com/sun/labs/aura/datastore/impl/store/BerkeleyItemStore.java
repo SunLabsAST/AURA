df6b8b8880e23fd2d72d5e2ad8298f2861174720
Stephen Green <stephen.green@sun.com>
2008-03-03 12:00:36 -0500

    Added findSimilar methods.

--- aardvark/src/com/sun/labs/aura/datastore/impl/store/BerkeleyItemStore.java
+++ aardvark/src/com/sun/labs/aura/datastore/impl/store/BerkeleyItemStore.java
@@ -2,6 +2,7 @@ package com.sun.labs.aura.datastore.impl.store;
 
 import com.sun.labs.aura.datastore.DBIterator;
 import com.sleepycat.je.DatabaseException;
+import com.sun.kt.search.WeightedField;
 import com.sun.labs.aura.AuraService;
 import com.sun.labs.aura.util.AuraException;
 import com.sun.labs.aura.datastore.Attention;
@@ -20,7 +21,6 @@ import com.sun.labs.util.props.ConfigBoolean;
 import com.sun.labs.util.props.ConfigComponent;
 import com.sun.labs.util.props.ConfigString;
 import com.sun.labs.util.props.Configurable;
-import com.sun.labs.util.props.ConfigurationEntries;
 import com.sun.labs.util.props.ConfigurationManager;
 import com.sun.labs.util.props.PropertyException;
 import com.sun.labs.util.props.PropertySheet;
@@ -305,6 +305,48 @@ public class BerkeleyItemStore implements Replicant, Configurable, AuraService {
             int count)
             throws AuraException, RemoteException {
         return bdb.getLastAttentionForUser(srcKey, type, count);
+    }
+    
+    /**
+     * Finds a the n most similar items to the given item.
+     * @param key the item that we want to find similar items for
+     * @param n the number of similar items to return
+     * @return the set of items most similar to the given item, ordered by 
+     * similarity to the given item.  The similarity of the items is based on 
+     * all of the indexed text associated with the item in the data store.
+     */
+    public SortedSet<Item> findSimilar(String key, int n) {
+        return null;
+    }
+
+    /**
+     * Finds the n most-similar items to the given item, based on the data in the 
+     * provided field.
+     * @param key the item for which we want similar items
+     * @param field the name of the field that should be used to find similar
+     * items
+     * @param n the number of similar items to return
+     * @return the set of items most similar to the given item, based on the 
+     * data indexed into the given field.  Note that the returned set may be
+     * smaller than the number of items requested!
+     */
+    public SortedSet<Item> findSimilar(String key, String field, int n) {
+        return null;
+    }
+    
+    /**
+     * Finds the n most-similar items to the given items, based on a combination
+     * of the data held in the provided fields.
+     * @param key the item for which we want similar items
+     * @param fields the fields (and associated weights) that we should use to 
+     * compute the similarity between items.
+     * @param n the number of similar items to return
+     * @return the set of items most similar to the given item, based on the data
+     * in the provided fields.   Note that the returned set may be
+     * smaller than the number of items requested!
+     */
+    public SortedSet<Item> findSimilar(String key, WeightedField[] fields, int n) {
+        return null;
     }
 
     public void addItemListener(ItemType itemType, ItemListener listener)
