3c17e82d31dd1fa6aa4778b4f198d3c2a3736236
Paul Lamere <Paul.Lamere@sun.com>
2008-03-24 10:09:29 -0400

    Adapted to new simpletimer constructor

--- aardvark/src/com/sun/labs/aura/aardvark/util/SimpleTimer.java
+++ aardvark/src/com/sun/labs/aura/aardvark/util/SimpleTimer.java
@@ -2,20 +2,24 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.sun.labs.aura.aardvark.util;
 
 public class SimpleTimer {
-    long last;
 
-    public SimpleTimer() {
+    long last;
+    boolean enabled;
+
+    public SimpleTimer(boolean enabled) {
         last = System.currentTimeMillis();
+        this.enabled = enabled;
     }
 
     public void mark(String msg) {
-        long now = System.currentTimeMillis();
-        long delta = now - last;
-        last = now;
-        System.out.println(msg + ": " + delta + " ms");
+        if (enabled) {
+            long now = System.currentTimeMillis();
+            long delta = now - last;
+            last = now;
+            System.out.println(msg + ": " + delta + " ms");
+        }
     }
 }
