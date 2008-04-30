6d31a312f82a338f423c8e1647456c1e007f6ca6
Paul Lamere <Paul.Lamere@sun.com>
2008-04-30 15:23:33 -0400

    Added a range filter that allows selecting items with a field that falls within a particular range

---
+++ aardvark/src/com/sun/labs/aura/aardvark/impl/recommender/FieldRangeFilter.java
@@ -0,0 +1,44 @@
+/*
+ * Project Aura,
+ * 
+ * Copyright (c) 2008,  Sun Microsystems Inc
+ * See license.txt for licensing info.
+ */
+package com.sun.labs.aura.aardvark.impl.recommender;
+
+import com.sun.kt.search.ResultAccessor;
+
+/**
+ *
+ * @author plamere
+ */
+public class FieldRangeFilter extends ResultsFilterAdapter {
+
+    private String fieldName;
+    private double minValue;
+    private double maxValue;
+
+    public FieldRangeFilter(String fieldName, double minValue, double maxValue) {
+        this.fieldName = fieldName;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    @Override
+    protected boolean lowLevelFilter(ResultAccessor ra) {
+        Object oVal = ra.getSingleFieldValue(fieldName);
+        if (oVal != null) {
+            System.out.println("oval class " + oVal.getClass().getSimpleName());
+            if (oVal instanceof Number) {
+                Number nVal = (Number) oVal;
+                double val = nVal.doubleValue();
+                System.out.printf("%f %f %f\n", val, minValue, maxValue);
+                return val >= minValue && val < maxValue;
+            } else {
+                System.out.printf("false %s\n", oVal.toString());
+                return false;
+            }
+        }
+        return false;
+    }
+}
