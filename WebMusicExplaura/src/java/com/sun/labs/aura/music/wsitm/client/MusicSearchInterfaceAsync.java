e9bd24e3c55b62d4bc39206805e4f0daa9a05c1e
Francois Maillet <francois.maillet@sun.com>
2008-05-29 17:12:26 -0400

    Fixed search by and for tag. Partial tag details done.

--- WebMusicExplaura/src/java/com/sun/labs/aura/music/wsitm/client/MusicSearchInterfaceAsync.java
+++ WebMusicExplaura/src/java/com/sun/labs/aura/music/wsitm/client/MusicSearchInterfaceAsync.java
@@ -18,9 +18,9 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface MusicSearchInterfaceAsync {
        public void artistSearch(String searchString, int maxResults, AsyncCallback callback) throws Exception;
        public void artistSearchByTag(String searchString, int maxResults, AsyncCallback callback) throws Exception; 
-       public void tagSearch(String searchString, int maxResults, AsyncCallback callback); 
+       public void tagSearch(String searchString, int maxResults, AsyncCallback callback) throws Exception; 
        public void getArtistDetails(String id, boolean refresh, AsyncCallback callback) throws Exception;
-       public void getTagDetails(String id, boolean refresh, AsyncCallback callback);
+       public void getTagDetails(String id, boolean refresh, AsyncCallback callback) throws Exception;
        public void getTagTree(AsyncCallback callback);
        public void getCommonTags(String artistID1, String artistID2, int num, AsyncCallback callback) throws Exception;
 }
