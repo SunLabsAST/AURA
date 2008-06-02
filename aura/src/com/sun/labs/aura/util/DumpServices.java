067c7708f08493c43b258f875c9c3ce344d16e21
Will Holcomb <will.holcomb@sun.com>
2008-06-02 16:57:58 -0400

    refactored display code into a new function

--- aura/src/com/sun/labs/aura/util/DumpServices.java
+++ aura/src/com/sun/labs/aura/util/DumpServices.java
@@ -7,34 +7,38 @@ package com.sun.labs.aura.util;
 
 import com.sun.labs.util.props.ComponentRegistry;
 import com.sun.labs.util.props.ConfigurationManager;
+import com.sun.labs.util.props.PropertyException;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import java.io.IOException;
 
 /**
  *
  * @author stgreen
  */
 public class DumpServices {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-
         URL cu = DumpServices.class.getResource(args[0]);
         if(cu == null) {
             cu = (new File(args[0])).toURI().toURL();
         }
-        ConfigurationManager cm = new ConfigurationManager(cu);
+        listServices(cu);
+    }
+    
+    public static void listServices(URL configURL) throws InterruptedException, PropertyException, IOException {
+        ConfigurationManager cm = new ConfigurationManager(configURL);
         ComponentRegistry cr = cm.getComponentRegistry();
         
-        Thread.sleep(10000);
         if(cr == null) {
             System.out.println("No component registry");
             return;
         }
+        Thread.sleep(10000);
         Map<String,List<String>> dump = cr.dumpJiniServices();
         for(Map.Entry<String,List<String>> e : dump.entrySet()) {
             System.out.println("Registrar: " + e.getKey());
@@ -42,7 +46,5 @@ public class DumpServices {
                 System.out.println("  Service: " + s);
             }
         }
-        // TODO code application logic here
     }
-
 }
