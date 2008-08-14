692df05a685a017d40ea733c5e1ff71c88a30891
jalex <jeffrey.alexander@sun.com>
2008-08-14 16:28:13 -0400

    Cleaned up a bit of config stuff - fixed process manager references in one config, and moved stopwords into aura since it shouldn't have a dependency on aardvark. Also added AuraShell for the same reason - now you can run a shell without aardvark.

---
+++ aura/src/com/sun/labs/aura/util/AuraShell.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.labs.aura.util;
+
+import com.sun.labs.aura.AuraService;
+import com.sun.labs.aura.datastore.DataStore;
+import com.sun.labs.util.command.CommandInterpreter;
+import com.sun.labs.util.props.ConfigComponent;
+import com.sun.labs.util.props.Configurable;
+import com.sun.labs.util.props.PropertyException;
+import com.sun.labs.util.props.PropertySheet;
+import java.util.logging.Logger;
+
+/**
+ * A simple wrapper that starts up a shell with the aura commands
+ */
+public class AuraShell implements AuraService, Configurable {
+    @ConfigComponent(type = DataStore.class)
+    public final static String PROP_DATA_STORE = "dataStoreHead";
+
+    @ConfigComponent(type = StatService.class)
+    public final static String PROP_STAT_SERVICE = "statService";
+
+    private DataStore dataStore;
+    private CommandInterpreter shell;
+    private Logger logger;
+    private ShellUtils sutils;
+    private StatService statService;
+
+    
+    public void start() {
+        shell = new CommandInterpreter();
+        shell.setPrompt("aura% ");
+        sutils = new ShellUtils(shell, dataStore, statService);
+        Thread t = new Thread() {
+
+            public void run() {
+                shell.run();
+                shell = null;
+            }
+        };
+        t.start();
+    }
+    
+    public void stop() {
+        if (shell != null) {
+            shell.close();
+        }
+    }
+    
+    /**
+     * Reconfigures this component
+     * @param ps the property sheet
+     * @throws com.sun.labs.util.props.PropertyException
+     */
+    public void newProperties(PropertySheet ps) throws PropertyException {
+        dataStore = (DataStore) ps.getComponent(PROP_DATA_STORE);
+        statService = (StatService) ps.getComponent(PROP_STAT_SERVICE);
+        logger = ps.getLogger();
+    }
+
+}
