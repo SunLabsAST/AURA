48fb0880851a0dc669b8ffbdd1c697969e8d4adf
Paul Lamere <Paul.Lamere@sun.com>
2008-10-12 12:56:22 -0400

    Refactored/simplified the DatabaseBroker to use the new RemoteComponentManager

--- SitmWebServices/src/java/com/sun/labs/aura/music/webservices/DatabaseBroker.java
+++ SitmWebServices/src/java/com/sun/labs/aura/music/webservices/DatabaseBroker.java
@@ -4,17 +4,12 @@
  */
 package com.sun.labs.aura.music.webservices;
 
-import com.sun.labs.aura.datastore.DataStore;
 import com.sun.labs.aura.music.MusicDatabase;
 import com.sun.labs.aura.util.AuraException;
-import com.sun.labs.util.props.Component;
-import com.sun.labs.util.props.ComponentListener;
 import com.sun.labs.util.props.ConfigurationManager;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collections;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletContext;
@@ -24,45 +19,35 @@ import javax.servlet.ServletContextListener;
 /**
  *
  */
-public class DatabaseBroker implements ServletContextListener, ComponentListener {
-
+public class DatabaseBroker implements ServletContextListener {
+    private final static String CONFIG_FILE_NAME = "/sitmWebConfig.xml";
     private final static String ATTRIBUTE_NAME = "databaseBroker";
-    protected Logger logger = Logger.getLogger("");
-    private ConfigurationManager cm = null;
+    private final static String DATASTORE_NAME = "dataStoreHead";
     private MusicDatabase mdb = null;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        Logger logger = Logger.getLogger("");
         logger.warning("Servlet DatabaseBroker is here!");
         try {
             ServletContext context = sce.getServletContext();
-            URL config = context.getResource("/sitmWebConfig.xml");
+            URL config = context.getResource(CONFIG_FILE_NAME);
             try {
-                cm = new ConfigurationManager();
+                ConfigurationManager cm = new ConfigurationManager();
                 cm.addProperties(config);
                 context.setAttribute(ATTRIBUTE_NAME, this);
+                mdb = new MusicDatabase(cm, DATASTORE_NAME);
             } catch (IOException ioe) {
                 logger.log(Level.SEVERE, "Can't load configuration " + config, ioe);
             }
+        } catch (AuraException ex) {
+            logger.severe("Can't connect to MusicDatabase " + ex.getMessage());
         } catch (MalformedURLException ex) {
             logger.severe("Bad URL to config file " + ex.getMessage());
         }
     }
 
     private MusicDatabase getMusicDatabase() {
-        if (mdb == null) {
-            DataStore store = (DataStore) cm.lookup("dataStoreHead", this);
-            if (store != null) {
-                try {
-                    logger.info("Servlet connecting to datastore " + store);
-                    mdb = new MusicDatabase(store);
-                } catch (AuraException ex) {
-                    logger.log(Level.SEVERE, "Servlet failed to connect to the datastore", ex);
-                }
-            } else {
-                logger.severe("No datastores available");
-            }
-        }
         return mdb;
     }
 
@@ -77,21 +62,5 @@ public class DatabaseBroker implements ServletContextListener, ComponentListener
     @Override
     public void contextDestroyed(ServletContextEvent arg0) {
         // TBD remove the lookup listener
-    }
-
-    @Override
-    public void componentAdded(Component c) {
-    }
-
-    @Override
-    public void componentRemoved(Component c) {
-        if (mdb != null) {
-            if (c instanceof DataStore) {
-                DataStore store = (DataStore) c;
-                if (mdb.getDataStore() == store) {
-                    mdb = null;
-                }
-            }
-        }
     }
 }
