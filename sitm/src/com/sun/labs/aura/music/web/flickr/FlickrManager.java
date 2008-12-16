0304e72c67dd68112a37198d9ade77ab3a8c136e
Paul Lamere <Paul.Lamere@sun.com>
2008-12-16 11:39:59 -0500

    Adjusted queries to improve proper image hits

--- sitm/src/com/sun/labs/aura/music/web/flickr/FlickrManager.java
+++ sitm/src/com/sun/labs/aura/music/web/flickr/FlickrManager.java
@@ -45,46 +45,50 @@ public class FlickrManager {
     public Image[] getPhotosForArtist(String name, int maxCount) {
         List<Image> imageList = new ArrayList<Image>();
 
-        imageList.addAll(getImagesByTags(normalizeName(name, true) + " concert", true, maxCount, true));
+        imageList.addAll(getImagesByTags(new String[] {normalizeName(name, false), "concert"}, true, maxCount, true));
+        //imageList.addAll(getImagesByTags(normalizeName(name, true) + " concert", true, maxCount, true));
 
         if (imageList.size() < maxCount) {
-            imageList.addAll(getImagesByTags(normalizeName(name, true), true, maxCount - imageList.size(), true));
+            imageList.addAll(getImagesByTags(new String[] {normalizeName(name, false), "show"}, true, maxCount - imageList.size(), true));
         }
 
         if (imageList.size() < maxCount) {
-            imageList.addAll(getImagesByTags(normalizeName(name, false) + " concert", true, maxCount - imageList.size(), true));
+            imageList.addAll(getImagesByTags(new String[] {normalizeName(name, false) }, true, maxCount - imageList.size(), true));
         }
 
         if (imageList.size() < maxCount) {
-            imageList.addAll(getImagesByTags(normalizeName(name, false), true, maxCount - imageList.size(), true));
+            imageList.addAll(getImagesByTags(normalizeName(name, false).split("\\s+"), true, maxCount - imageList.size(), true));
         }
 
         if (imageList.size() < maxCount) {
-            imageList.addAll(getImagesByTags(normalizeName(name, false), false, maxCount - imageList.size(), true));
+            imageList.addAll(getImagesByTags(new String[] {normalizeName(name, true) }, true, maxCount - imageList.size(), true));
         }
+
         return imageList.toArray(EMPTY);
     }
 
     private String normalizeName(String name, boolean removeSpace) {
+        name = name.toLowerCase();
         if (removeSpace) {
+            name = name.replaceAll("^the\\s+", "");
             name = name.replaceAll(" ", "");
         }
         name = name.replaceAll("&", " ");
         name = name.replaceAll("\\s+and\\s+", " ");
         name = name.replaceAll(",", " ");
-        name = name.toLowerCase();
-        name = name.replaceFirst("\\s+the\\s+", "");
+        //name = name.replaceFirst("\\s+the\\s+", "");
         return name;
     }
 
-    private List<Image> getImagesByTags(String text, boolean all, int max, boolean interesting) {
+
+    private List<Image> getImagesByTags(String[] tags, boolean all, int max, boolean interesting) {
         Flickr flickr = new Flickr(FLICKR_ID);
         flickr.setSharedSecret("f7f20b719f360458");
         PhotosInterface photosInterface = flickr.getPhotosInterface();
 
         List<Image> list = new ArrayList<Image>(max);
 
-        if (text.length() == 0) {
+        if (tags.length == 0) {
             return list;
         }
 
@@ -93,8 +97,11 @@ public class FlickrManager {
         }
 
         SearchParameters search = new SearchParameters();
-        String[] tags = text.split("\\s+");
         search.setTags(tags);
+        for (String s : tags) {
+            System.out.print("'" + s + "' ");
+        }
+        System.out.println("");
         //search.setText(text);
 
         if (ONLY_ATTRIBUTION_LICENSE) {
@@ -173,85 +180,94 @@ public class FlickrManager {
         if (images.length > 0) {
             totalGoodArtists++;
         }
-        if (false) {
+        if (true) {
             for (Image image : images) {
                 image.dump();
             }
         }
     }
 
+    private static void troublesomeArtists(FlickrManager fm) {
+        dumpPhotos(fm, "The Dogs D'Amour");
+    }
+
+    private static void troublesomeArtists2(FlickrManager fm) {
+        dumpPhotos(fm, "Stevie Ray Vaughan and Double Trouble");
+        dumpPhotos(fm, "Thelonious Monk");
+        dumpPhotos(fm, "They Might Be Giants");
+        dumpPhotos(fm, "The Byrds");
+        dumpPhotos(fm, "Hans Zimmer & Lisa Gerrard");
+    }
+
+    private static void troublesomeInternationalArtists(FlickrManager fm) {
+        dumpPhotos(fm, "Motörhead");
+        dumpPhotos(fm, "Kari Rueslåtten");
+        dumpPhotos(fm, "Jürgen Drews");
+        dumpPhotos(fm, "Röyksopp");
+        dumpPhotos(fm, "Boards of Canada");
+        dumpPhotos(fm, "Motörhead");
+        dumpPhotos(fm, "Bob Marley & The Wailers");
+        dumpPhotos(fm, "Nick Drake");
+        dumpPhotos(fm, "Die Ärzte");
+        dumpPhotos(fm, "Enya");
+        dumpPhotos(fm, "Beyoncé");
+        dumpPhotos(fm, "The Jimi Hendrix Experience");
+        dumpPhotos(fm, "Télépopmusik");
+        dumpPhotos(fm, "José González");
+        dumpPhotos(fm, "Blue Öyster Cult");
+        dumpPhotos(fm, "Cat Stevens");
+        dumpPhotos(fm, "Céline Dion");
+        dumpPhotos(fm, "Nightmares on Wax");
+        dumpPhotos(fm, "Everything but the Girl");
+        dumpPhotos(fm, "Mötley Crüe");
+        dumpPhotos(fm, "Michael Bublé");
+        dumpPhotos(fm, "Maxïmo Park");
+        dumpPhotos(fm, "The Byrds");
+        dumpPhotos(fm, "Sinéad O'Connor");
+        dumpPhotos(fm, "Hans Zimmer");
+        dumpPhotos(fm, "Hans Zimmer & Lisa Gerrard");
+        dumpPhotos(fm, "Fila Brazillia");
+        dumpPhotos(fm, "Ill Niño");
+        dumpPhotos(fm, "They Might Be Giants");
+        dumpPhotos(fm, "Kruder & Dorfmeister");
+        dumpPhotos(fm, "Dr. Dre & Eminem");
+        dumpPhotos(fm, "Émilie Simon");
+        dumpPhotos(fm, "Missy Elliott");
+        dumpPhotos(fm, "Frédéric Chopin");
+        dumpPhotos(fm, "Funki Porcini");
+        dumpPhotos(fm, "The Notorious B.I.G.");
+        dumpPhotos(fm, "Stevie Ray Vaughan and Double Trouble");
+        dumpPhotos(fm, "Boozoo Bajou");
+        dumpPhotos(fm, "Franz Schubert");
+        dumpPhotos(fm, "Gang Starr");
+        dumpPhotos(fm, "Sarah Brightman");
+        dumpPhotos(fm, "Felix Mendelssohn");
+        dumpPhotos(fm, "Emerson, Lake & Palmer");
+        dumpPhotos(fm, "Smoke City");
+        dumpPhotos(fm, "Deep Forest");
+        dumpPhotos(fm, "Georg Friedrich Händel");
+        dumpPhotos(fm, "Sly & The Family Stone");
+        dumpPhotos(fm, "Nitin Sawhney");
+        dumpPhotos(fm, "Eva Cassidy");
+        dumpPhotos(fm, "Queensrÿche");
+        dumpPhotos(fm, "Crosby, Stills, Nash & Young");
+        dumpPhotos(fm, "Thelonious Monk");
+        dumpPhotos(fm, "Peggy Lee");
+        dumpPhotos(fm, "?????");
+        dumpPhotos(fm, "Claude Debussy");
+        dumpPhotos(fm, "Kelly Rowland");
+        dumpPhotos(fm, "The Mamas & The Papas");
+        dumpPhotos(fm, "Julie London");
+        dumpPhotos(fm, "Róisín Murphy");
+        dumpPhotos(fm, "Café Tacuba");
+        dumpPhotos(fm, "Mo' Horizons");
+    }
+
     public static void main(String[] args) {
         FlickrManager fm = new FlickrManager();
-
-        if (false) {
-            dumpPhotos(fm, "Stevie Ray Vaughan and Double Trouble");
-            dumpPhotos(fm, "Thelonious Monk");
-            dumpPhotos(fm, "They Might Be Giants");
-            dumpPhotos(fm, "The Byrds");
-            dumpPhotos(fm, "Hans Zimmer & Lisa Gerrard");
-        } else {
-            dumpPhotos(fm, "Motörhead");
-            dumpPhotos(fm, "Kari Rueslåtten");
-            dumpPhotos(fm, "Jürgen Drews");
-            dumpPhotos(fm, "Röyksopp");
-            dumpPhotos(fm, "Boards of Canada");
-            dumpPhotos(fm, "Motörhead");
-            dumpPhotos(fm, "Bob Marley & The Wailers");
-            dumpPhotos(fm, "Nick Drake");
-            dumpPhotos(fm, "Die Ärzte");
-            dumpPhotos(fm, "Enya");
-            dumpPhotos(fm, "Beyoncé");
-            dumpPhotos(fm, "The Jimi Hendrix Experience");
-            dumpPhotos(fm, "Télépopmusik");
-            dumpPhotos(fm, "José González");
-            dumpPhotos(fm, "Blue Öyster Cult");
-            dumpPhotos(fm, "Cat Stevens");
-            dumpPhotos(fm, "Céline Dion");
-            dumpPhotos(fm, "Nightmares on Wax");
-            dumpPhotos(fm, "Everything but the Girl");
-            dumpPhotos(fm, "Mötley Crüe");
-            dumpPhotos(fm, "Michael Bublé");
-            dumpPhotos(fm, "Maxïmo Park");
-            dumpPhotos(fm, "The Byrds");
-            dumpPhotos(fm, "Sinéad O'Connor");
-            dumpPhotos(fm, "Hans Zimmer");
-            dumpPhotos(fm, "Hans Zimmer & Lisa Gerrard");
-            dumpPhotos(fm, "Fila Brazillia");
-            dumpPhotos(fm, "Ill Niño");
-            dumpPhotos(fm, "They Might Be Giants");
-            dumpPhotos(fm, "Kruder & Dorfmeister");
-            dumpPhotos(fm, "Dr. Dre & Eminem");
-            dumpPhotos(fm, "Émilie Simon");
-            dumpPhotos(fm, "Missy Elliott");
-            dumpPhotos(fm, "Frédéric Chopin");
-            dumpPhotos(fm, "Funki Porcini");
-            dumpPhotos(fm, "The Notorious B.I.G.");
-            dumpPhotos(fm, "Stevie Ray Vaughan and Double Trouble");
-            dumpPhotos(fm, "Boozoo Bajou");
-            dumpPhotos(fm, "Franz Schubert");
-            dumpPhotos(fm, "Gang Starr");
-            dumpPhotos(fm, "Sarah Brightman");
-            dumpPhotos(fm, "Felix Mendelssohn");
-            dumpPhotos(fm, "Emerson, Lake & Palmer");
-            dumpPhotos(fm, "Smoke City");
-            dumpPhotos(fm, "Deep Forest");
-            dumpPhotos(fm, "Georg Friedrich Händel");
-            dumpPhotos(fm, "Sly & The Family Stone");
-            dumpPhotos(fm, "Nitin Sawhney");
-            dumpPhotos(fm, "Eva Cassidy");
-            dumpPhotos(fm, "Queensrÿche");
-            dumpPhotos(fm, "Crosby, Stills, Nash & Young");
-            dumpPhotos(fm, "Thelonious Monk");
-            dumpPhotos(fm, "Peggy Lee");
-            dumpPhotos(fm, "?????");
-            dumpPhotos(fm, "Claude Debussy");
-            dumpPhotos(fm, "Kelly Rowland");
-            dumpPhotos(fm, "The Mamas & The Papas");
-            dumpPhotos(fm, "Julie London");
-            dumpPhotos(fm, "Róisín Murphy");
-            dumpPhotos(fm, "Café Tacuba");
-            dumpPhotos(fm, "Mo' Horizons");
-        }
+        troublesomeArtists(fm);
+        troublesomeArtists2(fm);
+        troublesomeInternationalArtists(fm);
 
         System.out.println("Total artists: " + totalArtists);
         System.out.println("Total good artists: " + totalGoodArtists);
