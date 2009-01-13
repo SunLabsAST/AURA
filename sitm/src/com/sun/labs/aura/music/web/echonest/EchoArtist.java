bc952beb33c8d21b58638c4e9ee6a15b7ee47cd8
Paul Lamere <Paul.Lamere@sun.com>
2009-01-13 13:14:29 -0500

    Represents Artist information as told by the echo nest

---
+++ sitm/src/com/sun/labs/aura/music/web/echonest/EchoArtist.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.labs.aura.music.web.echonest;
+
+/**
+ *
+ * @author plamere
+ */
+public class EchoArtist {
+    private String name;
+    private String id;
+    private String mbid;
+
+    public EchoArtist(String name, String id, String mbid) {
+        this.name = name;
+        this.id = id;
+        this.mbid = mbid;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMbid() {
+        return mbid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EchoArtist other = (EchoArtist) obj;
+        if ((this.id == null) ? (other.id != null) : !this.id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
