20f185e7b969e72c7250c5f48a9e495224f40dff
Paul Lamere <Paul.Lamere@sun.com>
2009-01-13 13:15:10 -0500

    Added code to get artist information from the echonest

--- sitm/src/com/sun/labs/aura/music/web/echonest/EchoNest.java
+++ sitm/src/com/sun/labs/aura/music/web/echonest/EchoNest.java
@@ -2,13 +2,20 @@
  *  Copyright (c) 2008, Sun Microsystems Inc.
  *  See license.txt for license.
  */
-
 package com.sun.labs.aura.music.web.echonest;
 
 import com.sun.labs.aura.music.web.Commander;
+import com.sun.labs.aura.music.web.XmlUtil;
+import com.sun.labs.aura.util.Scored;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -17,17 +24,151 @@ import org.w3c.dom.NodeList;
  * @author plamere
  */
 public class EchoNest {
+
     private Commander commander;
-    private String API_KEY="EHY4JJEGIOFA1RCJP";
+    private String API_KEY = "EHY4JJEGIOFA1RCJP";
     private final static int MAX_ROWS = 15;
-    
+
     public EchoNest() throws IOException {
-        commander = new Commander("last.fm", "http://developer.echonest.com/api/", "&api_key=" + API_KEY);
-        commander.setRetries(1);
+        commander = new Commander("EchoNest", "http://developer.echonest.com/api/", "&api_key=" + API_KEY);
+        commander.setRetries(0);
         commander.setTimeout(10000);
         commander.setTraceSends(false);
         commander.setTrace(false);
-        commander.setMinimumCommandPeriod(1000);
+        commander.setMinimumCommandPeriod(0);
+    }
+
+    public List<EchoArtist> artistSearch(String query) throws IOException {
+        List<EchoArtist> artists = new ArrayList<EchoArtist>();
+        String cmdURL = "search_artists?query=" + encode(query);
+        Document doc = commander.sendCommand(cmdURL);
+        Element docElement = doc.getDocumentElement();
+        Element artistList = (Element) XmlUtil.getDescendent(docElement, "artists");
+        if (artistList != null) {
+            NodeList itemList = artistList.getElementsByTagName("artist");
+            for (int i = 0; i < itemList.getLength(); i++) {
+                Element item = (Element) itemList.item(i);
+                String name = XmlUtil.getDescendentText(item, "name");
+                String enid = XmlUtil.getDescendentText(item, "id");
+                String mbid = XmlUtil.getDescendentText(item, "mbid");
+                EchoArtist artist = new EchoArtist(name, enid, mbid);
+                artists.add(artist);
+            }
+        }
+        return artists;
+    }
+
+    private String encode(String parameter) {
+        try {
+            return URLEncoder.encode(parameter, "UTF-8");
+        } catch (UnsupportedEncodingException ex) {
+            return parameter;
+        }
+    }
+
+    public EchoArtist getProfileByName(String qname) throws IOException {
+        String cmdURL = "get_profile?name=" + encode(qname);
+        Document doc = commander.sendCommand(cmdURL);
+        Element docElement = doc.getDocumentElement();
+        NodeList itemList = docElement.getElementsByTagName("artist");
+        if (itemList.getLength() == 1) {
+            Element item = (Element) itemList.item(0);
+            String name = XmlUtil.getDescendentText(item, "name");
+            String enid = XmlUtil.getDescendentText(item, "id");
+            String mbid = XmlUtil.getDescendentText(item, "mbid");
+            EchoArtist artist = new EchoArtist(name, enid, mbid);
+            return artist;
+        } else if (itemList.getLength() > 1) {
+            System.err.println("Woah - mroe than one artist return, which was unexpected");
+        }
+        return null;
+    }
+
+    public float getFamiliarity(String id) throws IOException {
+        float familiarity = 0f;
+        String cmdURL = "get_familiarity?id=" + id;
+        Document doc = commander.sendCommand(cmdURL);
+        Element docElement = doc.getDocumentElement();
+        Element artistElement = XmlUtil.getFirstElement(docElement, "artist");
+        String sFam = XmlUtil.getDescendentText(artistElement, "familiarity");
+        if (sFam != null) {
+            familiarity = Float.parseFloat(sFam);
+        }
+        return familiarity;
+    }
+
+    public float getHotness(String id) throws IOException {
+        float hotness = 0f;
+        String cmdURL = "get_hotttnesss?id=" + id;
+        Document doc = commander.sendCommand(cmdURL);
+        Element docElement = doc.getDocumentElement();
+        Element artistElement = XmlUtil.getFirstElement(docElement, "artist");
+        String sFam = XmlUtil.getDescendentText(artistElement, "hotttnesss");
+        if (sFam != null) {
+            hotness = Float.parseFloat(sFam);
+        }
+        return hotness;
+    }
+
+    public List<Scored<EchoArtist>> getTopHotttArtists(int count) throws IOException {
+        List<Scored<EchoArtist>> artists = new ArrayList<Scored<EchoArtist>>();
+
+        String cmdURL = "get_top_hottt_artists?rows=" + count;
+
+        Document doc = commander.sendCommand(cmdURL);
+        Element docElement = doc.getDocumentElement();
+        NodeList itemList = docElement.getElementsByTagName("artist");
+        for (int i = 0; i < itemList.getLength(); i++) {
+            Element item = (Element) itemList.item(i);
+            String name = XmlUtil.getDescendentText(item, "name");
+            String id = XmlUtil.getDescendentText(item, "id");
+            String mbid = XmlUtil.getDescendentText(item, "mbid");
+            String sHotness = XmlUtil.getDescendentText(item, "hotttnesss");
+            double hotness = 1;
+
+            if (sHotness != null) {
+                hotness = Double.parseDouble(sHotness);
+            }
+
+            EchoArtist artist = new EchoArtist(name, id, mbid);
+            artists.add(new Scored<EchoArtist>(artist, hotness));
+        }
+        return artists;
+    }
+
+    public List<Scored<EchoArtist>> getSimilarArtists(String id, int start, int count) throws IOException {
+        String cmdURL = "get_similar?id=" + id + "&start=" + start + "&rows=" + count;
+        return fetchSimilarArtists(cmdURL);
+    }
+
+    public List<Scored<EchoArtist>> getSimilarArtistsByName(String name, int start, int count) throws IOException {
+        String cmdURL = "get_similar?name=" + name + "&start=" + start + "&rows=" + count;
+        return fetchSimilarArtists(cmdURL);
+    }
+
+    private List<Scored<EchoArtist>> fetchSimilarArtists(String url) throws IOException {
+        List<Scored<EchoArtist>> artists = new ArrayList<Scored<EchoArtist>>();
+
+        Document doc = commander.sendCommand(url);
+        Element docElement = doc.getDocumentElement();
+        Element similar = (Element) XmlUtil.getDescendent(docElement, "similar");
+        NodeList itemList = similar.getElementsByTagName("artist");
+        for (int i = 0; i < itemList.getLength(); i++) {
+            Element item = (Element) itemList.item(i);
+            String name = XmlUtil.getDescendentText(item, "name");
+            String enid = XmlUtil.getDescendentText(item, "id");
+            String mbid = XmlUtil.getDescendentText(item, "mbid");
+            String srank = XmlUtil.getDescendentText(item, "rank");
+
+            int rank = 1;
+            if (srank != null) {
+                rank = Integer.parseInt(srank);
+            }
+
+            EchoArtist artist = new EchoArtist(name, enid, mbid);
+            artists.add(new Scored<EchoArtist>(artist, 1.0 / rank));
+        }
+        return artists;
     }
 
     /**
@@ -63,16 +204,66 @@ public class EchoNest {
         return results;
     }
 
+    private String encodeName(String artistName) {
+        try {
+            String encodedName = URLEncoder.encode(artistName, "UTF-8");
+            return encodedName;
+        } catch (UnsupportedEncodingException e) {
+            return "";
+        }
+    }
+
     public static void dump(EchoNest nest, String mbid) throws IOException {
         for (String url : nest.getAudio(mbid, 15)) {
             System.out.printf("%s %s\n", mbid, url);
         }
     }
 
+    public void crawl(String path) throws IOException {
+        Set<String> visited = new HashSet<String>();
+        List<EchoArtist> todo = new LinkedList<EchoArtist>();
+
+        List<Scored<EchoArtist>> seeds = getTopHotttArtists(15);
+        for (Scored<EchoArtist> sartist : seeds) {
+            todo.add(sartist.getItem());
+        }
+
+        PrintWriter out = new PrintWriter(path);
+        try {
+            while (todo.size() > 0) {
+                EchoArtist artist = todo.remove(0);
+                if (!visited.contains(artist.getId())) {
+                    visited.add(artist.getId());
+                    float familiarity = getFamiliarity(artist.getId());
+                    float hotness = getHotness(artist.getId());
+                    System.out.printf("Visited:%d/%d  Familiarity %6.4f Hotness: %6.4f Artist:%s\n",
+                            visited.size(), todo.size(), familiarity, hotness, artist.toString());
+                    List<Scored<EchoArtist>> similar = getSimilarArtists(artist.getId(), 0, 15);
+                    for (Scored<EchoArtist> sim : similar) {
+                        EchoArtist simArtist = sim.getItem();
+                        System.out.println("   " + sim.getItem().getName());
+                        if (!visited.contains(sim.getItem().getId())) {
+                            todo.add(sim.getItem());
+                        }
+                        out.printf("%s|%s|%6.4f|%6.4f|%s|%s\n", artist.getId(), artist.getName(), familiarity, hotness,
+                                simArtist.getId(), simArtist.getName());
+                    }
+                }
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException e) {
+                }
+            }
+        } finally {
+            out.close();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         EchoNest echoNest = new EchoNest();
-        dump(echoNest, "11eabe0c-2638-4808-92f9-1dbd9c453429");
-        dump(echoNest, "83d91898-7763-47d7-b03b-b92132375c47");
-        dump(echoNest, "299278d3-25dd-4f30-bae4-5b571c28034d");
+        echoNest.crawl("echocrawl.txt");
+    //dump(echoNest, "11eabe0c-2638-4808-92f9-1dbd9c453429");
+    //dump(echoNest, "83d91898-7763-47d7-b03b-b92132375c47");
+    //dump(echoNest, "299278d3-25dd-4f30-bae4-5b571c28034d");
     }
 }
