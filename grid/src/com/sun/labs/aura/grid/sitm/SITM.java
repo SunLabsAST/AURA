b0f59757ee7c9cd8a7d2a8bb31376ce4adcce123
Stephen Green <stephen.green@sun.com>
2009-06-11 13:48:12 -0400

    Use standard artistCrawlerConfig.xml, which doesn't limit the number of artists.

--- grid/src/com/sun/labs/aura/grid/sitm/SITM.java
+++ grid/src/com/sun/labs/aura/grid/sitm/SITM.java
@@ -56,7 +56,7 @@ public abstract class SITM extends ServiceAdapter {
             "-DcacheDir=" + GridUtil.cacheFSMntPnt,
             "-jar",
             GridUtil.auraDistMntPnt + "/dist/grid.jar",
-            "/com/sun/labs/aura/music/resource/artistCrawler20KConfig.xml",
+            "/com/sun/labs/aura/music/resource/artistCrawlerConfig.xml",
             "starter",
             String.format("%s/sitm/artistCrawler.%%g.out", GridUtil.logFSMntPnt)
         };
