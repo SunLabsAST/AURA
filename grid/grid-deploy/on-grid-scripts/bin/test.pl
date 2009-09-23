e22adc8bfbbb6445812d69d72115f0dbf8ebd5cc
Stephen Green <stephen.green@sun.com>
2009-09-23 10:40:40 -0400

    Added script to upload and unzip SunStudio tools on-grid.

--- grid/grid-deploy/on-grid-scripts/bin/test.pl
+++ grid/grid-deploy/on-grid-scripts/bin/test.pl
@@ -2,9 +2,8 @@
 #
 # Launch a collector-wrapped jvm as a test.
 $| = 1;
-print `/usr/bin/chmod 0777 /files/dev-tools/sunstudio12.1/bin/*`;
 print `echo \"$0\" \"$SHELL\"`;
-print `/usr/bin/ls -lh /files/dev-tools/sunstudio12.1/bin`;
+print `/usr/bin/ls -lh /files/dev-tools/SunStudioExpress/bin`;
 
 
     
